a0e5d87e976f110baef1cb4c620ddb6a045ff01b
Veniel Dorestave <veniel@noukod.com>
2019-01-11 20:20:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,8 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get('/name'){"My name is Veniel"
+  get('/hometown'){"My hometown is pestel"
+  get('/favorite-song'){"My favorite song is kompa
+"
 end
